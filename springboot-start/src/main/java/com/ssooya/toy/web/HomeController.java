6e1e5a97060ce1048e5d65786ee4518326b3ba5a
ssooya90 <ssooya90@naver.com>
2020-02-20 08:52:29 +0900

    springboot - start 포트변경 및 hello world 출력

--- springboot-start/src/main/java/com/ssooya/toy/web/HomeController.java
+++ springboot-start/src/main/java/com/ssooya/toy/web/HomeController.java
@@ -2,17 +2,19 @@ package com.ssooya.toy.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by ssooya90@naver.com on 2020-02-19
  * Github : http://github.com/ssooya90
  */
 
-@Controller
+@RestController
 public class HomeController {
 
 	@RequestMapping("/")
-	public String index(){
-		return "Hello Wolrd!";
+	public String home() {
+		return "Hello, Spring boot!";
 	}
+
 }
