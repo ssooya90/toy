bd26706729eed0490910cc95c52918fa403a213c
ssooya90 <ssooya90@naver.com>
2020-02-21 16:00:04 +0900

    JPA 기본설정 진행중

--- springboot-start/src/main/java/com/ssooya/toy/config/JpaConfig.java
+++ springboot-start/src/main/java/com/ssooya/toy/config/JpaConfig.java
@@ -1,9 +1,8 @@
-package com.ssooya.toy.domain;
+package com.ssooya.toy.config;
 
 /**
  * Created by ssooya90@naver.com on 2020-02-21
  * Github : http://github.com/ssooya90
  */
-
-public class Member {
+public class JpaConfig {
 }
---
+++ springboot-start/src/main/java/com/ssooya/toy/domain/member/Member.java
@@ -0,0 +1,29 @@
+package com.ssooya.toy.domain.member;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Created by ssooya90@naver.com on 2020-02-21
+ * Github : http://github.com/ssooya90
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idx;
+
+	@Column(nullable = false) // 널허용 X
+	private String userId;
+
+	@Column(nullable = false)
+	private String password;
+
+}
