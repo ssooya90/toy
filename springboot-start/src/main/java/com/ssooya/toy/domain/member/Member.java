3c58f55608418fb67d13166141e774c3a33d6aff
ssooya90 <ssooya90@naver.com>
2020-02-24 01:06:43 +0900

    member test code 작성중

--- springboot-start/src/main/java/com/ssooya/toy/domain/member/Member.java
+++ springboot-start/src/main/java/com/ssooya/toy/domain/member/Member.java
@@ -1,11 +1,8 @@
 package com.ssooya.toy.domain.member;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+		import lombok.*;
 
-import javax.persistence.*;
+		import javax.persistence.*;
 
 /**
  * Created by ssooya90@naver.com on 2020-02-21
@@ -18,7 +15,7 @@ public class Member {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idx;
+	private Long id;
 
 	@Column(nullable = false) // 널허용 X
 	private String userId;
@@ -34,5 +31,14 @@ public class Member {
 
 	@Column
 	private String userAge;
+
+	@Builder
+	public Member(String userId, String password, String userName, String userType, String userAge){
+		this.userId = userId;
+		this.password = password;
+		this.userName = userName;
+		this.userType = userType;
+		this.userAge = userAge;
+	}
 
 }
---
+++ springboot-start/src/test/java/com/ssooya/toy/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.ssooya.toy.domain.member;
+
+// Member Entity Test Class
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@After // 단위 테스트가 끝날 때마다 수행되는 메소드를 지정,
+	public void cleanup() {
+		memberRepository.deleteAll();
+	}
+
+	@Test
+	public void 멤버_불러오기(){
+
+		//given
+		String userId = "ssooya90";
+		String password = "1234";
+
+		memberRepository.save(Member.builder()
+				.userId(userId)
+				.password(password)
+				.build());
+
+
+		//when
+		List<Member> memberList = memberRepository.findAll();
+
+		//then
+		Member member = memberList.get(0);
+		assertThat(member.getUserId()).isEqualTo(userId);
+		assertThat(member.getPassword()).isEqualTo(password);
+	}
+}
