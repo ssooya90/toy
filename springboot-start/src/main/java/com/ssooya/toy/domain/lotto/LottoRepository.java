0606bad7af5c5703529070224a06b7542f777915
ssooya90 <ssooya90@naver.com>
2020-04-15 15:25:00 +0900

    lotto db레이어 계층 생성

---
+++ springboot-start/src/main/java/com/ssooya/toy/domain/lotto/LottoRepository.java
@@ -0,0 +1,13 @@
+package com.ssooya.toy.domain.lotto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// DB Layer 접근자
+// <Entity Class, PK타입> 상속
+public interface LottoRepository extends JpaRepository<Lotto, Long> {
+
+	Optional<Lotto> findById(String id);
+
+}
