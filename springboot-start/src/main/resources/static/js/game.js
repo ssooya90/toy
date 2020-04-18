e8b91b2dab462ef33767ddf495eb3933ad292d44
ssooya90 <ssooya90@naver.com>
2020-04-18 10:46:16 +0900

    노트북에서는 왜 접속이 안될까?

--- springboot-start/src/main/resources/static/js/game.js
+++ springboot-start/src/main/resources/static/js/game.js
@@ -1,13 +1,12 @@
 var game = {
 
-	init : function () {
+	init: function () {
 		var _this = this;
 
 		this.lottoMake();
 
 
-
-		$('#writeMoveBtn').on('click',function () {
+		$('#writeMoveBtn').on('click', function () {
 
 			_this.write_move();
 		});
@@ -15,48 +14,40 @@ var game = {
 	},
 
 
-	selectChange : function (e) {
+	selectChange: function (e) {
 
-		var _parents = 	$('.numberSelect');
+		var _parents = $('.numberSelect');
 		var _cnt = 0;
 
 
 		console.log("1")
 
-		$.each(_parents.find('input[type=checkBox]'),function (index, item) {
+		$.each(_parents.find('input[type=checkBox]'), function (index, item) {
 
 			$(this).prop("checked") ? _cnt++ : '';
 		});
 
 
-
-
-		if(_cnt == 7){
+		if (_cnt == 7) {
 
 			alert("6개까지 선택이 가능합니다.");
-			$(e).prop("checked",false);
+			$(e).prop("checked", false);
 		}
-
-
-
-
-
-
 
 	},
 
 	// 로또 생성
-	lottoMake : function () {
+	lottoMake: function () {
 
-		var _parents = 	$('.numberSelect');
+		var _parents = $('.numberSelect');
 		var _str = "";
 
-		for(var i = 1 ; i <= 45 ; i ++){
+		for (var i = 1; i <= 45; i++) {
 
-			_str +=  '<div class="numBox">';
+			_str += '<div class="numBox">';
 			_str += '	<input type="checkbox" name="lottoNum" value="' + i + '" id="num' + i + '" onchange="game.selectChange(this)">';
 			_str += '	<label for="num' + i + '">';
-			_str += '		<span class="numTxt">' + i +'</span>';
+			_str += '		<span class="numTxt">' + i + '</span>';
 			_str += '	</label>';
 			_str += '</div>';
 
@@ -64,7 +55,9 @@ var game = {
 
 		_parents.append(_str);
 
-}
+	}
+
+
 };
 
 game.init();
