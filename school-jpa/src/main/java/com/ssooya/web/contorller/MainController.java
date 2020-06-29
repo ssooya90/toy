15310b667ee71d2e61f4894efa11943b68ee7dc7
ssooya <ssooya90@naver.com>
2020-06-30 08:55:35 +0900

    Feat : thymeleaf layout 설정

--- school-jpa/src/main/java/com/ssooya/web/contorller/MainController.java
+++ school-jpa/src/main/java/com/ssooya/web/contorller/MainController.java
@@ -1,9 +1,7 @@
 package com.ssooya.web.contorller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
@@ -12,7 +10,7 @@ public class MainController {
 	@RequestMapping("/")
 	public String home() {
 
-		return "index";
+		return "page/index";
 	}
 
 }
