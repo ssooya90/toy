31a1dfd60beadf6f187a742ff29670aedd75ade3
ssooya <ssooya90@naver.com>
2020-08-13 09:01:47 +0900

    Feat 게시글 등록 및 리스트 출력

--- spring-jpa-webservice/src/main/java/com/ssooya/webservice/service/PostsService.java
+++ spring-jpa-webservice/src/main/java/com/ssooya/webservice/service/PostsService.java
@@ -2,12 +2,16 @@ package com.ssooya.webservice.service;
 
 import com.ssooya.webservice.domain.posts.PostRepository;
 import com.ssooya.webservice.domain.posts.Posts;
+import com.ssooya.webservice.web.dto.PostsListResponseDto;
 import com.ssooya.webservice.web.dto.PostsResponseDto;
 import com.ssooya.webservice.web.dto.PostsSaveRequestDto;
 import com.ssooya.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,10 @@ public class PostsService {
 		Posts entity = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 		return new PostsResponseDto(entity);
 
+	}
+
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc(){
+		return postRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
 	}
 }
--- spring-jpa-webservice/src/main/java/com/ssooya/webservice/web/dto/IndexController.java
+++ spring-jpa-webservice/src/main/java/com/ssooya/webservice/web/dto/IndexController.java
@@ -1,14 +1,27 @@
 package com.ssooya.webservice.web.dto;
 
+import com.ssooya.webservice.service.PostsService;
 import jdk.nashorn.internal.objects.annotations.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+	private final PostsService postsService;
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+
+		model.addAttribute("posts",postsService.findAllDesc());
 		return "index";
+	}
+
+	@GetMapping("/posts/save")
+	public String postsSave(){
+		return "posts-save";
 	}
 }
