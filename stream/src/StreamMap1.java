8ae9a250e10d91f6feeec0817ab50deaeea91fed
ssooya <ssooya90@naver.com>
2021-01-29 09:09:19 +0900

    Feat Stream Map 사용

--- stream/src/StreamMap1.java
+++ stream/src/StreamMap1.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.*;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
+
 /*
 	타입형 스트림
  */
@@ -19,18 +21,22 @@ public class StreamMap1 {
 	public static void main(String[] args) {
 
 		AtomicInteger index = new AtomicInteger(0);
-
 		List<Integer> car = Arrays.asList(1, 2, 3);
-
 		Map<Integer, String> map = car.stream()
 				.collect(Collectors.toMap(
 						i1 -> car.get(index.get()),        // key
-						i2 -> car.get(index.incrementAndGet()).toString(),        // value
-						(existingValue, newValue) -> existingValue)
+						i2 -> car.get(index.getAndIncrement()).toString(),        // value
+						(existingValue, newValue) -> existingValue)    // 중복키가 발생할 경우 처리 방법
 				);
 
 
-		System.out.println("TEST");
+		List<String> names = Arrays.asList("java 8", "teSt");
+		List<?> namesStream = names.stream()
+				.map(name -> name.toUpperCase())
+				.collect(Collectors.toList());
+
+
+		System.out.println(names);
 
 
 	}
