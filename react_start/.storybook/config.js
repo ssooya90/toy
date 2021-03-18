44ad10d2265535d5848d6f8e18da4e8f6a376f03
ssooya90 <ssooya90@naver.com>
2021-03-18 09:05:12 +0900

    Revert "Feat 스타일 컴포넌트 활용"
    This reverts commit 45ddcab1

--- react_start/.storybook/config.js
+++ react_start/.storybook/config.js
@@ -1,15 +1,10 @@
 
-import {configure, setAddon} from "@storybook/react";
-import JSXAddon from 'storybook-addon-jsx';
+import {configure} from "@storybook/react";
 
 function loadStories(){
 
-
 	require('../src/stories/InputStory');
 	// 스토리 파일 여기 추가
-	require('../src/stories/NewCounterStory');
-	require('../src/stories/TextStory');
 }
 
-setAddon(JSXAddon)
 configure(loadStories, module);
