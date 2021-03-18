4224f32d226cf76f29dc8d2bf8a71ebd5908c844
ssooya <ssooya90@naver.com>
2021-03-18 09:07:20 +0900

    fix git 수정중

---
+++ react_start/src/App.js
@@ -0,0 +1,17 @@
+import './materialize.scss'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<nav>
+			<div className="nav-wrapper">
+				<div>두잇! 리액트 시작하2기</div>
+			</div>
+		</nav>
+		<h1>머터리얼</h1>
+    </div>
+  );
+}
+
+export default App;
