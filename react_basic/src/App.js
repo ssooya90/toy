5aa56011a42eafaefcebccfae9b5a867d4489785
ssooya90 <ssooya90@naver.com>
2021-03-18 09:18:27 +0900

    git 이상함

---
+++ react_basic/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+		<div>
+			<div>안녕하세요.</div>
+		</div>
+
+
+    );
+  }
+}
+
+export default App;
