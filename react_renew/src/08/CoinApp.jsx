cccfc65f47ad34f091b68cde92b07a78f7f39b58
ssooya90 <ssooya90@naver.com>
2021-04-12 08:51:54 +0900

    Feat React 가상코인거래소 추가

---
+++ react_renew/src/08/CoinApp.jsx
@@ -0,0 +1,12 @@
+import React, {PureComponent} from 'react';
+import AppLayout from './components/AppLayout';
+
+class CoinApp extends PureComponent {
+  render () {
+    return (
+      <AppLayout>본문</AppLayout>
+    );
+  }
+}
+
+export default CoinApp;
---
+++ react_renew/src/stories/ModalStory.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import Modal from '../06/Modal';
+import Button from '../04/Button';
+import Text from '../04/Text';
+import ButtonWithModal from '../06/ButtonWithModal';
+
+storiesOf('Modal',module)
+.addWithJSX('기본 설정',() => (
+
+  <Modal>
+    <div>
+      <Text>
+        정말로 삭제하시껫습니까?
+      </Text>
+    </div>
+    <Button primary>예</Button>
+    <Button>닫기</Button>
+  </Modal>
+
+))
+.addWithJSX('ButtonWithModal', () => <ButtonWithModal/>);
