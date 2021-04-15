54f4050cab849647d249c7da17e18d7124299825
ssooya90 <ssooya90@naver.com>
2021-04-15 09:02:30 +0900

    Feat 리덕스 초기작업

--- react_renew/src/08/CoinApp.jsx
+++ react_renew/src/08/CoinApp.jsx
@@ -1,10 +1,29 @@
 import React, {PureComponent} from 'react';
+import {Provider} from 'react-redux';
+
+
 import AppLayout from './components/AppLayout';
+import CoinOverview from './components/main/CoinOverview';
+import TransactionList from './components/main/TransactionList';
+import configureStore from './store/configureStore';
+
 
 class CoinApp extends PureComponent {
+
+  store = configureStore();
+
   render () {
     return (
-      <AppLayout>본문</AppLayout>
+
+      <Provider store={this.store}>
+        <AppLayout>
+          <CoinOverview/>
+          <TransactionList/>
+        </AppLayout>
+
+
+      </Provider>
+
     );
   }
 }
