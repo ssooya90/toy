ad4a218c2a61f325a2527ca47668cd054ce1e17c
ssooya90 <ssooya90@naver.com>
2021-06-10 09:01:58 +0900

    Feat 알고리즘 3

---
+++ algorism_study/src/hash/HashTest3.java
@@ -0,0 +1,37 @@
+package hash;
+
+import java.util.HashMap;
+
+public class HashTest3 {
+
+
+
+
+	public static void main(String[] args) {
+
+		String[][] phone_book = {{"동그란 안경","검정 선글라스"},{"파란색 티셔츠"},{"청바지"},{"긴 코트"}};
+
+		solution(phone_book);
+
+	}
+
+
+	public static int solution(String[][] phone_book) {
+
+		int answer = 0;
+		HashMap<String, Integer> map = new HashMap<>();
+
+		System.out.println(phone_book);
+
+		for(String name[] : phone_book){
+			System.out.println(name);
+
+			for(String name2 : name){
+				System.out.println(name2);
+			}
+		}
+
+		return answer;
+	}
+
+}
