6eab2b8dfd809ad89e3617ca946a4b85b699c26e
ssooya90 <ssooya90@naver.com>
2021-06-23 09:04:02 +0900

    Feat 완전탐색 알고리즘 진행중

--- algorism_study/src/완전탐색/모의고사.java
+++ algorism_study/src/완전탐색/모의고사.java
@@ -1,13 +1,13 @@
 package 완전탐색;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class 모의고사 {
 
 	public static void main(String[] args) {
 
-		int[] array = {1, 3, 2, 4, 2}; // 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열
+		int[] array = {1, 2, 3, 4, 5}; // 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열
 		solution(array);
 
 	}
@@ -15,59 +15,54 @@ public class 모의고사 {
 
 	public static int[] solution(int[] answers) {
 
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
 		int[] man1 = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
 		int[] man2 = {2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5};
 		int[] man3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5,};
 
-		int man1Score = 0, man2Score = 0, man3Score = 0;
+		int[] score = {0,0,0};
 
+		int manMaxScore[] = new int[3];
+		int[] answer = new int[3];
+
+
+		System.out.println("!");
 		for(int i = 0 ; answers.length > i ; i++){
 
 			if(answers[i] == man1[i]){
-				man1Score ++;
+				score[0]++;
+				manMaxScore[0] ++;
 			}
 
 			if(answers[i] == man2[i]){
-				man2Score ++;
+				score[1]++;
+				manMaxScore[1] ++;
 			}
 
 			if(answers[i] == man3[i]){
-				man3Score ++;
+				score[2]++;
+				manMaxScore[2] ++;
 			}
 		}
 
-
 		// man1,2,3 숫자중 가장 높은 숫자를 선정 후, 동점자 있을 경우 오름차순
 
+		// 최대값
+//		(manMaxScore);
+		Arrays.sort(manMaxScore);
 
-//
-//		for(int num : answers){
-//
-//			System.out.println(num);
-//
-//
-//		}
+		int max = manMaxScore[manMaxScore.length-1];
+		System.out.println(max);
 
+//		manScore[0];
+		for(int i = 0 ; score.length > i ; i++){
 
+			if(score[i] == max){
+				answer[i] = i+1;
+			}
 
-//		for(int num : answers){
-//
-//			if(man1[cnt] == num){
-//				man1Score ++;
-//			}
-//
-//			if(man2[cnt] == num){
-//				man2Score ++;
-//			}
-//
-//			if(man3[cnt] == num){
-//				man3Score ++;
-//			}
-//
-//		}
-//
-		int[] answer = {};
-
+		}
 
 		return answer;
 
