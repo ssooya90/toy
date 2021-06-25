bcb6751669c855bf40b493b7833ebddf8a5e8cd8
ssooya90 <ssooya90@naver.com>
2021-06-25 09:05:34 +0900

    Feat 탐욕법 체육복 알고리즘

---
+++ algorism_study/src/탐욕법/체육복.java
@@ -0,0 +1,24 @@
+package 탐욕법;
+
+public class 체육복 {
+
+	public static void main(String[] args) {
+
+		int[] lost = {2,4}; // 도난
+		int[] reserve = {1,3,5}; // 여벌
+		int n = 5; // 전체 학생수 (2명 이상 30명 이하)
+
+		solution(n, lost, reserve);
+
+	}
+
+
+	public static int solution(int n, int[] lost, int[] reserve) {
+
+		int answer = 0;
+		return answer;
+
+	}
+	
+	
+}
