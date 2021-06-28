bec380a8ca657eb52353c4068dab362a2784f780
ssooya90 <ssooya90@naver.com>
2021-06-28 09:07:43 +0900

    Feat 탐욕법 체육복 알고리즘

--- algorism_study/src/탐욕법/체육복.java
+++ algorism_study/src/탐욕법/체육복.java
@@ -1,5 +1,8 @@
 package 탐욕법;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 체육복 {
 
 	public static void main(String[] args) {
@@ -12,11 +15,70 @@ public class 체육복 {
 
 	}
 
-
 	public static int solution(int n, int[] lost, int[] reserve) {
 
-		int answer = 0;
+		int answer = n;
+
+		List<Integer> list = new ArrayList<Integer>();
+
+		for(int i : lost){
+
+			boolean pass = true;	// 패스값 설정
+			boolean loan = false;
+
+			System.out.println("i값" + i);
+
+			for(int j : reserve){
+
+
+				System.out.println("j값" + j);
+
+
+				for(int k : list){
+					System.out.println("K리스트" + k);
+
+					 System.out.println("이때 j값은?" + j);
+
+					if(k == j){
+
+						System.out.println("포함");
+						pass = false;
+						break;
+					}
+				}
+
+				if(pass){
+					if(i == j-1) {
+						list.add(j);
+						loan = true;
+						break;
+					}else if(i == j + 1){
+						list.add(j);
+						loan = true;
+						break;
+					}
+				}
+			}
+
+			if(loan == false){
+				answer --;
+			}
+
+		}
+
+
+		for(int i : list){
+			System.out.println("최종k : " + i);
+		}
+
+		System.out.println(answer);
+
 		return answer;
+
+
+
+
+
 
 	}
 	
