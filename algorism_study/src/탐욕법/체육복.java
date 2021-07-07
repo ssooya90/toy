8037cdd0fb30c018049f093eaec56c3bf2ddbc82
ssooya90 <ssooya90@naver.com>
2021-07-07 09:00:04 +0900

    Feat 탐욕법 체육복 알고리즘

--- algorism_study/src/탐욕법/체육복.java
+++ algorism_study/src/탐욕법/체육복.java
@@ -7,9 +7,9 @@ public class 체육복 {
 
 	public static void main(String[] args) {
 
-		int[] lost = {2,4}; // 도난
+		int[] lost = {1,2}; // 도난
 		int[] reserve = {1,3,5}; // 여벌
-		int n = 5; // 전체 학생수 (2명 이상 30명 이하)
+		int n = 3; // 전체 학생수 (2명 이상 30명 이하)
 
 		solution(n, lost, reserve);
 
@@ -21,34 +21,41 @@ public class 체육복 {
 
 		List<Integer> list = new ArrayList<Integer>();
 
+//		 여벌이 있으나 도난 당한 경우엔 빌려줄 수 없으므로 선처리
+//		for(int i : lost){
+//
+//			for(int j : reserve){
+//
+//				if(i == j){
+//					list.add(j);
+//					break;
+//				}
+//			}
+//		}
+
+
+
 		for(int i : lost){
 
-			boolean pass = true;	// 패스값 설정
-			boolean loan = false;
+			boolean loan = false; // 빌림 유무
 
-			System.out.println("i값" + i);
 
 			for(int j : reserve){
 
-
-				System.out.println("j값" + j);
-
+				boolean pass = true;  // 패스값 설정
 
 				for(int k : list){
-					System.out.println("K리스트" + k);
-
-					 System.out.println("이때 j값은?" + j);
 
 					if(k == j){
 
-						System.out.println("포함");
+						System.out.println("있는 빌려준 적이 있음" +  j);
 						pass = false;
 						break;
 					}
 				}
 
 				if(pass){
-					if(i == j-1) {
+					if(i == j - 1) {
 						list.add(j);
 						loan = true;
 						break;
@@ -56,16 +63,21 @@ public class 체육복 {
 						list.add(j);
 						loan = true;
 						break;
+					}else if(i == j){
+						list.add(j);
+						loan = true;
+						break;
 					}
 				}
 			}
 
+			// 결국 못 빌렸다면 수업을 들 수 있는 학생 -1
 			if(loan == false){
+				System.out.println("앤써 깍아?");
 				answer --;
 			}
 
 		}
-
 
 		for(int i : list){
 			System.out.println("최종k : " + i);
@@ -76,11 +88,11 @@ public class 체육복 {
 		return answer;
 
 
-
+//
 
 
 
 	}
-	
-	
+
+
 }
